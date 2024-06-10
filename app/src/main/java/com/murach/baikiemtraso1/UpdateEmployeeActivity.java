b734c71841903497233d19ee7767955cc38d6457
Le Tuan <leminhtuanthpttranhungdaotx@gmail.com>
2024-06-10 22:40:41 +0700

    all view success

---
+++ app/src/main/java/com/murach/baikiemtraso1/UpdateEmployeeActivity.java
@@ -0,0 +1,14 @@
+package com.murach.baikiemtraso1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_employee);
+    }
+}
